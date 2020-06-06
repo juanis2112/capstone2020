d18d65b593b8e6e563733fbc0ce3a38b406c2135
juanksRuiz <33558038+juanksRuiz@users.noreply.github.com>
2020-06-06 18:13:22 -0500

    codigo datos de prueba

---
+++ datos_prueba/clase_estudiante.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+class Estudiante:
+    def __init__(self,doc_ingreso, doc_actual,codigo,
+                 apellido1, apellido2, nombre, acceso,
+                 subacceso, correo, sexo, facultad,
+                 semestre,creditos_inscritos,asignaturas,inscritas):
+        
+        self.doc_ingreso = doc_ingreso
+        self.doc_actual = doc_actual
+        self.codigo = codigo
+        self.apellido1 = apellido1
+        self.apellido2 = apellido2
+        self.nombre = nombre
+        self.acceso = acceso
+        self.subacceso = acceso
+        self.correo = correo
+        self.sexo = sexo
+        self.facultado = facultad
+        self.semestre = semestre
+        self.creditos_inscritos = creditos_inscritos
+        self.asignaturas_inscritas
+    def getInfoAsignaturas(self):
+        
+    
